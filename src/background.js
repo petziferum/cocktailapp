933e75c22bf742cdf10d2ef967b18f860e02ec72
Petzi <daboarderpjb@gmail.com>
2020-03-29 18:38:01 +0200

    fullscreen

--- src/background.js
+++ src/background.js
@@ -19,7 +19,7 @@ function createWindow () {
   win = new BrowserWindow({
     width: 800,
     height: 600,
-    fullscreen:false,
+    fullscreen:true,
     frame:false,
     autoHideMenuBar: true,
     kiodk: true,
